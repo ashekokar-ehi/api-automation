e77cdca7bb5393c08cf8f0839f6a57b92968f0dc
Akshay Shekokar <AShekokar@corp.evolenthealth.com>
2020-09-17 13:06:06 +0530

    added lodash

--- validate-response.js
+++ validate-response.js
@@ -1,21 +1,22 @@
+const _ = require("lodash");
 const validateResponse = (() => {
-    return (matchType, actualResponse, expectedResponse) => {
-        console.log("hello",matchType, actualResponse, expectedResponse);
-        if(matchType === 'matchFull'){
-            return _.isEqual(actualResponse, expectedResponse);
-          } else if(matchType === 'contains'){
-            return checkIfContains(actualResponse,expectedResponse);
-          }
+  return (matchType, actualResponse, expectedResponse) => {
+    console.log("hello", matchType, actualResponse, expectedResponse);
+    if (matchType === "matchFull") {
+      return _.isEqual(actualResponse, expectedResponse);
+    } else if (matchType === "contains") {
+      return checkIfContains(actualResponse, expectedResponse);
     }
+  };
 })();
 
 const checkIfContains = (mainObj, subObj) => {
-    return Object.keys(subObj).every(ele => {
-        if (typeof subObj[ele] == 'object') {
-            return checkIfContains(mainObj[ele], subObj[ele]);
-        }
-        return subObj[ele] === mainObj[ele]
-    });
-  }
+  return Object.keys(subObj).every((ele) => {
+    if (typeof subObj[ele] == "object") {
+      return checkIfContains(mainObj[ele], subObj[ele]);
+    }
+    return subObj[ele] === mainObj[ele];
+  });
+};
 
 module.exports = validateResponse;
